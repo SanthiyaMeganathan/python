c4861bf70a73446b3d5133ac81fa1f32a9dfcc9e
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-06-23 14:02:47 +0530

    hi

---
+++ calculator.py
@@ -0,0 +1,12 @@
+num1 = input("enter the num")
+num2 = input("enter the num")
+ 
+result = int(num1)+int(num2)
+print(result)
+
+num1 = input("enter the num")
+num2 = input("enter the num")
+
+result2 = float(num1)+float(num2)
+print(result2)
+
