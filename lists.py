1262a11334f7a7015b9dcb46a3d93b0c2cfece64
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-06-24 13:51:54 +0530

    hlo

---
+++ lists.py
@@ -0,0 +1,13 @@
+friends =["ammu","tommy","jhon", "hari","yogi"]
+fav_num =["1","2","3","4"]
+print(friends)
+print(friends[3])
+
+friends[1] = "kevin"
+print(friends[1])
+
+friends.extend(fav_num)
+print(friends)
+
+friends.append("creed")
+print(friends)
