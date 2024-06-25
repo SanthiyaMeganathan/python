7e3ac82302d24d6fbf985c815f1446647a43ab00
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-06-25 08:11:15 +0530

    hlo

---
+++ return.py
@@ -0,0 +1,14 @@
+def cube(num):
+    print(num*num*num)
+cube(8)    
+    
+
+
+def cube(num):
+    return num*num*num
+print(cube(8))
+
+def cube(num):
+    return num*num*num
+result = cube(8)
+print(result)
