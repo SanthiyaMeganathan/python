1920894f788a0710e3e3015d8a4e696f12f8d3ed
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-06-25 12:09:21 +0530

    msg

---
+++ forloop.py
@@ -0,0 +1,17 @@
+for name in "abcd":
+    print(name)
+
+
+array = ["hi","helo","say", "hmm"]
+length =len(array)
+print(length)
+for name in array:
+    print(name)
+
+
+for i in range (10):
+    print(i)
+
+for i in range (0,10) :
+    print(i)
+
