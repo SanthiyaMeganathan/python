a41e187579e9fd0e615ef7fefbfa2302e5df2de4
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-06-25 08:00:50 +0530

    sg

---
+++ functions.py
@@ -0,0 +1,5 @@
+def sayhi(name , age):
+    print("Hello, user!  you are " + name  +  str(age))
+sayhi("mike" , 2)  
+#sayhi() #this will give an error because the function is expecting an argument
+sayhi("nicola" ,"3" )
