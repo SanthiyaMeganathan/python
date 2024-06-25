e1b247d182b2d1aecd61be509f104e5b4cc6df1b
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-06-25 17:02:46 +0530

    msg

---
+++ powerfunction/2dlist.py
@@ -0,0 +1,12 @@
+two_dimlist =[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [10]
+]
+
+print(two_dimlist[1][1])
+
+for rows in two_dimlist:
+    for col in rows:
+        print(col)
---
+++ powerfunction/exponentialfunction.py
@@ -0,0 +1,8 @@
+
+
+def raise_power(base_num , pow_num):
+    result =1
+    for index in range (pow_num):
+        result = result*base_num
+    return result
+print(raise_power(3,2))        
