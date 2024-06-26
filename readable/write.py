ba0b00655fc740538d457a32232e6f2c51b36ae0
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-06-26 07:36:56 +0530

    msg

--- readable/write.py
+++ readable/write.py
@@ -3,3 +3,19 @@ employee_name = open("readable/textfile.txt","w")
 employee_name.write('shanto - md')
 
 employee_name.close()
+#shanto - md'
+
+
+employee_name = open("readable/textfile1.txt","w")
+
+employee_name.write('shanto - md')
+
+employee_name.close()
+#shanto - md'
+
+employee_name = open("readable/index.html","w")
+
+employee_name.write('<p>shanto - md</p>')
+
+employee_name.close()
+#shanto - md'
